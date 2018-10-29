fd2f359cbe270a6ec6e6869aa6401614fff69ea8
rheafernandes <rheafernandeshallu@gmail.com>
2018-10-29 11:56:39 +0530

    added webpack config files and component navbar

---
+++ src/app/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import AppRouter from './AppRouter';
+
+
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
+
+console.log("Hello World");
